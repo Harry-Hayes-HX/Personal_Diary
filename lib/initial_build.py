d9d953f41140070a73bb4b0a51e8a94d5da53cb5
Harry-Hayes-HX <137900746+Harry-Hayes-HX@users.noreply.github.com>
2023-07-25 13:29:55 +0100

    init files

--- lib/initial_build.py
+++ lib/initial_build.py
@@ -1,5 +1,5 @@
 def make_snippet(string):
-    sentence = string
-    first_five = sentence.split()[:5]
+    sentence = string.split()
+    first_five = " ".join(sentence[:5])
     return first_five + "..."
 make_snippet("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque condimentum rutrum enim non volutpat. Maecenas pretium faucibus placerat. Sed nec placerat nisi. Pellentesque tellus velit, elementum non orci a, porta tempor sapien.")
--- tests/test_make_snippet.py
+++ tests/test_make_snippet.py
@@ -3,4 +3,4 @@ from lib.initial_build import *
 
 def test_snippets():
     build = make_snippet("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque condimentum rutrum enim non volutpat. Maecenas pretium faucibus placerat. Sed nec placerat nisi. Pellentesque tellus velit, elementum non orci a, porta tempor sapien.")
-    assert build == "Lorem ipsum dolor sit amet, consectetur..."
+    assert build == "Lorem ipsum dolor sit amet,..."
